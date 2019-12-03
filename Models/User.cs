da61276bef8717350d60b246c785535ef054ff5e
Arman-prog <58466722+Arman-prog@users.noreply.github.com>
2019-12-03 14:10:22 +0400

    Initial commit

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }       
+        public string PhoneNumber1 { get; set; }      
+        public string PhoneNumber2 { get; set; }
+    }
+}
